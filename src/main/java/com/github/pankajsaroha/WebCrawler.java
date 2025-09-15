77f05d497435b527a459b2571e87b4db47f87840
pankajsaroha <pk89651@gmail.com>
2025-09-15 21:57:34 +0530

    Updated Crawler with Fixed, Virtual and Platform threads

--- src/main/java/com/github/pankajsaroha/WebCrawler.java
+++ src/main/java/com/github/pankajsaroha/WebCrawler.java
@@ -1,17 +1,24 @@
 package com.github.pankajsaroha;
 
+import com.github.pankajsaroha.context.CrawlerContext;
 import com.github.pankajsaroha.crawler.Crawler;
+import com.github.pankajsaroha.crawler.Fetcher;
+import com.github.pankajsaroha.crawler.Parser;
 import com.github.pankajsaroha.fontier.InMemoryFrontierQueue;
+import com.github.pankajsaroha.storage.FileContentStorage;
+import com.github.pankajsaroha.storage.UrlMetadataStorage;
+
 
 import java.io.IOException;
 
 public class WebCrawler {
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Hello world!");
         InMemoryFrontierQueue queue = new InMemoryFrontierQueue();
         queue.enqueue("https://example.com/");
 
-        Crawler crawler = new Crawler(queue);
-        crawler.parse("https://example.com/");
+        Crawler crawler = new Crawler(new CrawlerContext(queue, new Fetcher(), new Parser(), new FileContentStorage(), new UrlMetadataStorage()), 10, Crawler.Mode.VIRTUAL_PER_TASK);
+        crawler.startCrawling();
+
+
     }
 }
--- src/main/java/com/github/pankajsaroha/crawler/Crawler.java
+++ src/main/java/com/github/pankajsaroha/crawler/Crawler.java
@@ -6,50 +6,76 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Crawler {
+    public enum Mode {
+        FIXED_PLATFORM_POOL,  // N platform threads
+        PLATFORM_PER_TASK,   // one platform thread per task
+        VIRTUAL_PER_TASK    // one virtual thread per task
+    }
     private HttpClient client;
     private CrawlerContext context;
     private ExecutorService executorService;
     private volatile boolean running;
     private int numThreads;
-    private boolean useVirtualThreads;
+    private Mode mode;
 
-    public Crawler(CrawlerContext context, int numThreads, boolean useVirtualThreads) {
+    public Crawler(CrawlerContext context, int numThreads, Mode mode) {
         this.context = context;
         this.client = HttpClient.newHttpClient();
         this.running = true;
         this.numThreads = numThreads;
-        this.useVirtualThreads = useVirtualThreads;
-        this.executorService = useVirtualThreads ? Executors.newVirtualThreadPerTaskExecutor()
-        :Executors.newFixedThreadPool(numThreads);
+        this.mode = mode;
+        //This will spawn as number of virtual threads as tasks but fixed platform threads.
+        //this.executorService = useVirtualThreads ? Executors.newVirtualThreadPerTaskExecutor() : Executors.newFixedThreadPool(numThreads);
+        this.executorService = switch (mode) {
+            case FIXED_PLATFORM_POOL -> Executors.newFixedThreadPool(numThreads);
+            case PLATFORM_PER_TASK -> Executors.newThreadPerTaskExecutor(Thread.ofPlatform().factory());
+            case VIRTUAL_PER_TASK -> Executors.newThreadPerTaskExecutor(Thread.ofVirtual().factory());
+        };
     }
 
+    /**
+     * We should never call .run() directly if you want the task to be executed by the executor’s threads — calling .run() just executes
+     * the code synchronously in the current thread, bypassing the executor entirely.
+     *
+     * new Worker(url, context).run();
+     * This runs the worker in the same thread that’s running workerLoop().
+     * So in FIXED_PLATFORM_POOL mode, that’s fine — because we want bounded concurrency (same thread which polls the url, executes it) and the loop itself is running inside the pool threads.
+     * But in PLATFORM_PER_TASK and VIRTUAL_PER_TASK modes, we’re already creating a new task per URL in startCrawling() so that executor service could map it to appropriate thread
+     * @throws InterruptedException
+     */
     public void startCrawling() throws InterruptedException {
-        if (useVirtualThreads) {
-            // With virtual threads, just spawn a task per URL
-            executorService.submit(() -> {
-                while (running) {
-                    try {
-                        String url = context.getQueue().take();
-                        executorService.submit(new Worker(url, context));
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                        break;
-                    }
+        switch (mode) {
+            case FIXED_PLATFORM_POOL -> {
+                //bounded concurrency — loop inside pool threads:
+                for (int i = 0; i < numThreads; i++) {
+                    executorService.submit(this::workerLoop);
                 }
-            });
-        } else {
-            for (var i = 0; i < numThreads; i++) {
+            }
+            case PLATFORM_PER_TASK, VIRTUAL_PER_TASK -> {
+                //Spawn a new thread for each URL
                 executorService.submit(() -> {
-                    while (running && !Thread.currentThread().isInterrupted()) {
+                    while (running) {
                         try {
                             String url = context.getQueue().take();
-                            new Worker(url, context).run();
+                            executorService.submit(new Worker(url, context));
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                             break;
                         }
                     }
                 });
+            }
+        }
+    }
+
+    private void workerLoop() {
+        while (running && !Thread.currentThread().isInterrupted()) {
+            try {
+                String url = context.getQueue().take();
+                new Worker(url, context).run();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
             }
         }
     }
@@ -64,5 +90,6 @@ public class Crawler {
         /*if (!executorService.awaitTermination(30, TimeUnit.SECONDS)) {
             executorService.shutdownNow(); // force stop if not finished
         }*/
+
     }
 }
